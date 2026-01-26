ddd49d494a658d46e4226617a68bfe10ff1c923d
Boulmehad Abderrahmane <boabmeuni01@gmail.com>
2026-01-26 23:44:31 +0100

    Remove unused imports from Products component and add redirects configuration

--- frontend/src/pages/sections/Products.jsx
+++ frontend/src/pages/sections/Products.jsx
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import { Flame } from "lucide-react";
-import prod1_img from "../../assets/images/hyperx_headphones.png";
-import prod2_img from "../../assets/images/genesis_mouse.png";
-import prod3_img from "../../assets/images/gruby_orange_headphones.png";
 export default function Products() {
   return (
     <>
